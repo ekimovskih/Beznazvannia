70ff0fdcc230e4d02c5ac126c505850ef47066ab
ekimovskih <43802399+ekimovskih@users.noreply.github.com>
2020-05-25 10:44:07 +0500

    First Enemy
    -Slimes!

---
+++ Assets/Scripts/Enemys/Enemy_propertys_scr.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_propertys_scr : MonoBehaviour
+{
+    public float Health = 100f;
+    public float Damage;
+    public float AgroRadius;
+    public float DisAgroRadius;
+    [HideInInspector] public bool Agred = false;
+    [HideInInspector] public float distance;
+    public float AttackRadius;
+    [HideInInspector] public Vector2 dirrection;
+    public bool CanAttack = true;
+    public GameObject AttackZone = null;
+    
+
+    public void IsAgred(GameObject player)
+    {
+        Vector3 ThisPos = this.gameObject.transform.position;
+        Vector3 PlayerPos = player.transform.position;
+        ThisPos = PlayerPos - ThisPos;
+        distance = Mathf.Sqrt(ThisPos.x * ThisPos.x + ThisPos.y * ThisPos.y);
+        if (distance < AgroRadius)
+        {
+            Agred = true;
+            dirrection = new Vector2(ThisPos.x / distance, ThisPos.y / distance);
+        }
+        else if (distance > DisAgroRadius)
+        {
+            Agred = false;
+        }
+
+    }
+
+    public void TakeDamage(float dmg)
+    {
+        Health -= dmg;
+    }
+}
---
+++ Assets/Scripts/Enemys/Slime_1_scr.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slime_1_scr : Enemy_propertys_scr
+{
+    // Start is called before the first frame update
+    private GameObject player = null;
+    private Vector3 playerPosition;
+    public float AttackSpeed = 2f;
+    public float MovementSpeed = 50f;
+    
+    void Start()
+    {
+        player = GameObject.Find("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.gameObject.transform.position = new Vector3(this.gameObject.transform.position.x, this.gameObject.transform.position.y, this.gameObject.transform.position.y);
+        IsAgred(player); // вместе со всем высчитывает и дисанцию до игрока
+        if (distance < AttackRadius)
+        {
+            if (CanAttack)
+            {
+                StartCoroutine(Attack());
+            }
+        }
+        else if (CanAttack && Agred)
+        {
+            StartCoroutine(Jump(dirrection));
+            //SimpleMovement();
+        }
+        else if (CanAttack && !Agred)
+        {
+            float valuex = Random.Range(-1f, 1f);
+            float valuey = Random.Range(-1f, 1f);
+            Debug.Log(valuex + " " + valuey);
+            float value = Mathf.Sqrt(valuex * valuex + valuey * valuey);
+            //value = 1f;
+            Vector2 NewDir = new Vector2(valuex / value, valuey / value);
+            StartCoroutine(Jump(NewDir));
+        }
+    }
+    
+    IEnumerator Attack()
+    {
+        CanAttack = false;
+        Debug.Log("Prepare");
+        yield return new WaitForSeconds(1f);
+        Debug.Log("Jump attack");
+        this.gameObject.GetComponent<Rigidbody2D>().AddForce(dirrection * AttackSpeed);
+        Debug.Log("Brfrfrfrf");
+        yield return new WaitForSeconds(3f);
+        CanAttack = true;
+    }
+    
+    IEnumerator Jump(Vector2 dir)
+    {
+        Debug.Log("Jump move");
+        CanAttack = false;
+        this.gameObject.GetComponent<Rigidbody2D>().AddForce(dir * MovementSpeed);
+        //transform.Translate(dirrection * Time.deltaTime);
+        yield return new WaitForSeconds(1f);
+        CanAttack = true;
+    }
+    void SimpleMovement()
+    {
+        transform.Translate(dirrection * Time.deltaTime * 1.5f);
+    }
+}
