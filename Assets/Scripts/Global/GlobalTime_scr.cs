77ec94070c8e0022128eeed820b5c047042d68c2
Федор Меньщиков <fedor-menshikov@yandex.ru>
2020-06-04 17:05:26 +0500

    Hp, clock
    Added hp bar and world clock

--- Assets/Scripts/Global/GlobalTime_scr.cs
+++ Assets/Scripts/Global/GlobalTime_scr.cs
@@ -1,13 +1,58 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
+using UnityEngine.UI;
 
-public class GlobalTime_scr : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
+public class GlobalTime_scr : MonoBehaviour {
+
+    public Text ampm;
+    public Text minutes;
+    public Text hours;
+    public string midday;
+    public float param;
+    public float second1;
+    public float minuta1;
+    public float hours1;
+
+    private void Start()
     {
-        //DontDestroyOnLoad(this.gameObject); // не тестил еще
+        midday = "am";
     }
 
+    void Update () {
+        param -= Time.deltaTime;
+        if (param <= 0)
+        {
+            param = 0.015f;
+            second1 = second1 + 1;
+        }
+
+        if (second1 >= 60)
+        {
+            minuta1 = minuta1 + 1;
+            second1 = 0;
+        }
+
+        if (minuta1 >= 60)
+        {
+            hours1 = hours1 + 1;
+            minuta1 = 0;
+        }
+
+        if (hours1 > 11)
+        {
+            hours1 = 1;
+            if (midday == "am")
+                midday = "pm";
+            else
+            {
+                midday = "am";
+            }
+        }
+
+        ampm.text = "" + midday;
+        minutes.text = "" + minuta1;
+        hours.text = "" + hours1 + ":";
+    }
 }
---
+++ Assets/Scripts/Player/HealthBar.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    public Image bar;
+    public float fill;
+    
+    void Start()
+    {
+        fill = 1f;
+    }
+    
+    void Update()
+    {
+        GameObject go = GameObject.Find("Player");
+        Player_movement_scr hpController = go.GetComponent<Player_movement_scr>();
+        float currentHP = hpController.Health;
+        fill = currentHP / 100; 
+        bar.fillAmount = fill;
+    }
+}
