4da5d8907291fbb316e46eca07d954ffad0a4180
ekimovskih <43802399+ekimovskih@users.noreply.github.com>
2020-06-12 17:18:34 +0500

    rfgdgd

---
+++ Assets/Sprites/Main menu/menu_button.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class menu_button : MonoBehaviour
+{
+    // Start is called before the first frame update
+ 
+    public void Exit()
+    {
+        Application.Quit();
+    }
+    public void LoadNewGame()
+    {
+        StartCoroutine(LoadGame());
+        
+    }
+    public void Mute()
+    {
+        AudioListener camera = GameObject.Find("Main Camera").GetComponent<AudioListener>();
+        camera.enabled = !camera.isActiveAndEnabled;
+    }
+    IEnumerator LoadGame()
+    {
+        GameObject.Find("DarkScreen").GetComponent<DarkScreen>().MakeDarker();
+        yield return new WaitForSeconds(1f);
+        SceneManager.LoadScene("Village_0");
+    }
+}
