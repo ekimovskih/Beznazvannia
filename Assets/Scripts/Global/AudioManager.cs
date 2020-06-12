b1f5c6fd2dcdf5470f4f535444881723e854fd5c
Федор Меньщиков <fedor-menshikov@yandex.ru>
2020-06-12 17:17:40 +0500

    fix

--- Assets/Scripts/Global/AudioManager.cs
+++ Assets/Scripts/Global/AudioManager.cs
@@ -18,49 +18,49 @@ public class AudioManager : MonoBehaviour
     {
         //Debug.Log(Agred.Length);
         int i = Random.Range(0, Agred.Length);
-        GetComponent<AudioSource>().PlayOneShot(Agred[i]);
+        GetComponent<AudioSource>().PlayOneShot(Agred[i], 0.5f);
     }
     
     public void PlayAudioDeath()
     {
         //Debug.Log(Agred.Length);
         int i = Random.Range(0, Death.Length);
-        GetComponent<AudioSource>().PlayOneShot(Death[i]);
+        GetComponent<AudioSource>().PlayOneShot(Death[i], 0.5f);
     }
     
     public void PlayAudioAttack()
     {
         //Debug.Log(Agred.Length);
         int i = Random.Range(0, Attack.Length);
-        GetComponent<AudioSource>().PlayOneShot(Attack[i]);
+        GetComponent<AudioSource>().PlayOneShot(Attack[i], 0.5f);
     }
     
     public void PlayAudioAggroLost()
     {
         //Debug.Log(Agred.Length);
         int i = Random.Range(0, AggroLost.Length);
-        GetComponent<AudioSource>().PlayOneShot(AggroLost[i]);
+        GetComponent<AudioSource>().PlayOneShot(AggroLost[i], 0.5f);
     }
     
     public void PlayAudioPickUp()
     {
         //Debug.Log(Agred.Length);
         int i = Random.Range(0, PickUp.Length);
-        GetComponent<AudioSource>().PlayOneShot(PickUp[i]);
+        GetComponent<AudioSource>().PlayOneShot(PickUp[i], 0.5f);
     }
     
     public void PlayAudioHit()
     {
         //Debug.Log(Agred.Length);
         int i = Random.Range(0, Hit.Length);
-        GetComponent<AudioSource>().PlayOneShot(Hit[i]);
+        GetComponent<AudioSource>().PlayOneShot(Hit[i], 0.5f);
     }
     
     public void PlayAudioMine()
     {
         //Debug.Log(Agred.Length);
         int i = Random.Range(0, Mine.Length);
-        GetComponent<AudioSource>().PlayOneShot(Mine[i]);
+        GetComponent<AudioSource>().PlayOneShot(Mine[i], 0.5f);
     }
 
 }
