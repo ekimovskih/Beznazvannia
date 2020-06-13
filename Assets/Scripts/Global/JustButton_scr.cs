65d9f1b21165318f58c3846d8da3c1482133bf92
ekimovskih <43802399+ekimovskih@users.noreply.github.com>
2020-06-13 19:51:20 +0500

    тутор

--- Assets/Scripts/Global/JustButton_scr.cs
+++ Assets/Scripts/Global/JustButton_scr.cs
@@ -2,10 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 public class JustButton_scr : MonoBehaviour
 {
     public int count=0;
     public Text text;
+    //public GameObject go;
+    //public float mod = 1;
+    //private Vector3 offset = Vector3.down;
+    //public bool off;
+    //private Button j;
+
+    //public void
     public void clickCounter()
     {
         count++;
---
+++ Assets/Scripts/Global/MenuButton.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuButton : MonoBehaviour
+{
+    public GameObject Tab;
+    public GameObject ButtonOn;
+    public GameObject ButtonOFF;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Tab.SetActive(!Tab.activeSelf);
+            ButtonOn.SetActive(!ButtonOn.activeSelf);
+            ButtonOFF.SetActive(!ButtonOFF.activeSelf);
+            //Debug.Log("sdfsdfsdf");
+        }
+    }
+}
